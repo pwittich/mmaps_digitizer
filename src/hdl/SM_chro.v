b7274d01a2f6adff0c73be17f1cf95e4f4934199
Peter Wittich <wittich@cornell.edu>
2016-09-26 09:58:07 -0400

    updates to state machines 	modified:   ../hdl/SM_chro.v 	new file:   spi_sm.fzm
    new SM for the SPI slave
    update the single channel state machine to try to get Quartus to recognize
    it as a state machine (Rerun fizzim with one-hot encoding.) not successful.

--- src/hdl/SM_chro.v
+++ src/hdl/SM_chro.v
@@ -1,9 +1,9 @@
 
-// Created by fizzim.pl version 5.10 on 2016:04:29 at 15:32:42 (www.fizzim.com)
+// Created by fizzim.pl version 5.10 on 2016:09:25 at 22:32:09 (www.fizzim.com)
 
 module multi_ro (
-  output wire CHSEL,
-  output wire WR_EN,
+  output reg CHSEL,
+  output reg WR_EN,
   input wire CLK,
   input wire DAVAIL,
   input wire RST 
@@ -11,68 +11,95 @@ module multi_ro (
 
   // state bits
   parameter 
-  IDLE         = 3'b000, // extra=0 WR_EN=0 CHSEL=0 
-  CH_SELECT    = 3'b011, // extra=0 WR_EN=1 CHSEL=1 
-  READOUT      = 3'b111, // extra=1 WR_EN=1 CHSEL=1 
-  WRITE_HEADER = 3'b010; // extra=0 WR_EN=1 CHSEL=0 
+  IDLE         = 0, 
+  CH_SELECT    = 1, 
+  READOUT      = 2, 
+  WRITE_HEADER = 3; 
 
-  (* syn_encoding = "user" *) reg [2:0] state;
-  reg [2:0] nextstate;
+  reg [3:0] state;
+  reg [3:0] nextstate;
 
   // comb always block
   always @* begin
-    nextstate = state; // default to hold value because implied_loopback is set
-    case (state)
-      IDLE        : begin
+    nextstate = 4'b0000;
+    case (1'b1) // synopsys parallel_case full_case
+      state[IDLE]        : begin
         if (DAVAIL) begin
-          nextstate = WRITE_HEADER;
-        end
-      end
-      CH_SELECT   : begin
-        begin
-          nextstate = READOUT;
-        end
-      end
-      READOUT     : begin
-        if (DAVAIL) begin
-          nextstate = READOUT;
+          nextstate[WRITE_HEADER] = 1'b1;
         end
         else begin
-          nextstate = IDLE;
+          nextstate[IDLE] = 1'b1; // Added because implied_loopback is true
         end
       end
-      WRITE_HEADER: begin
+      state[CH_SELECT]   : begin
         begin
-          nextstate = CH_SELECT;
+          nextstate[READOUT] = 1'b1;
+        end
+      end
+      state[READOUT]     : begin
+        if (DAVAIL) begin
+          nextstate[READOUT] = 1'b1;
+        end
+        else begin
+          nextstate[IDLE] = 1'b1;
+        end
+      end
+      state[WRITE_HEADER]: begin
+        begin
+          nextstate[CH_SELECT] = 1'b1;
         end
       end
     endcase
   end
 
-  // Assign reg'd outputs to state bits
-  assign CHSEL = state[0];
-  assign WR_EN = state[1];
-
   // sequential always block
   always @(posedge CLK) begin
     if (RST)
-      state <= IDLE;
+      state <= 4'b0001 << IDLE;
     else
       state <= nextstate;
+  end
+
+  // datapath sequential always block
+  always @(posedge CLK) begin
+    if (RST) begin
+      CHSEL <= 0;
+      WR_EN <= 0;
+    end
+    else begin
+      CHSEL <= 0; // default
+      WR_EN <= 0; // default
+      case (1'b1) // synopsys parallel_case full_case
+        nextstate[IDLE]        : begin
+          ; // case must be complete for onehot
+        end
+        nextstate[CH_SELECT]   : begin
+          CHSEL <= 1;
+          WR_EN <= 1;
+        end
+        nextstate[READOUT]     : begin
+          CHSEL <= 1;
+          WR_EN <= 1;
+        end
+        nextstate[WRITE_HEADER]: begin
+          WR_EN <= 1;
+        end
+      endcase
+    end
   end
 
   // This code allows you to see state names in simulation
   `ifndef SYNTHESIS
   reg [95:0] statename;
   always @* begin
-    case (state)
-      IDLE        :
+    case (1'b1)
+      state[IDLE]        :
         statename = "IDLE";
-      CH_SELECT   :
+      state[CH_SELECT]   :
         statename = "CH_SELECT";
-      READOUT     :
+      state[READOUT]     :
         statename = "READOUT";
-      WRITE_HEADER:
+      state[WRITE_HEADER]:
         statename = "WRITE_HEADER";
       default     :
         statename = "XXXXXXXXXXXX";
