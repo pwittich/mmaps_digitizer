e8700210e5e891eb740f100d818c4023b5e20e01
Peter Wittich <wittich@cornell.edu>
2016-10-03 21:20:13 -0400

    Update toplevel to include SPI SM - first pass, based on spi_demo_sm.sv - fifo command is wrong (needs an additional state )

--- src/hdl/maaps_daq_toplevel.v
+++ src/hdl/maaps_daq_toplevel.v
@@ -12,67 +12,67 @@
 // Altera or its authorized distributors.  Please refer to the 
 // applicable agreement for further details.
 
-// PROGRAM		"Quartus II 64-Bit"
-// VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Full Version"
-// CREATED		"Fri Jun 24 17:07:43 2016"
+// PROGRAM              "Quartus II 64-Bit"
+// VERSION              "Version 13.1.0 Build 162 10/23/2013 SJ Full Version"
+// CREATED              "Fri Jun 24 17:07:43 2016"
 `timescale 1ns / 1ps
 `default_nettype none
   // Top level HDL module
   module maaps_daq_toplevel(
-			    CK50,
-			    adcfastclk_p,
-			    adcframe_p,
-				 SPI_MOSI,
-				 SPI_MISO,
-				 SPI_SCLK,
-				 SPI_SS,
-			    PMT_trigger, // external trigger - how to input? not in the SDC file now
-			    adcdata_p,
-			    TDC,
-			    ADC_SCLK1,
-			    ADC_nCS1,
-			    ADCCLK1_p,
-			    ADCCLK2_p,
-			    L1,
-			    L0,
-			    ADC_SDIO1,
-			    );
+                            CK50,
+                            adcfastclk_p,
+                            adcframe_p,
+                                 SPI_MOSI,
+                                 SPI_MISO,
+                                 SPI_SCLK,
+                                 SPI_SS,
+                            PMT_trigger, // external trigger - how to input? not in the SDC file now
+                            adcdata_p,
+                            TDC,
+                            ADC_SCLK1,
+                            ADC_nCS1,
+                            ADCCLK1_p,
+                            ADCCLK2_p,
+                            L1,
+                            L0,
+                            ADC_SDIO1,
+                            );
 
 
-   input wire	CK50;
-   input wire	adcfastclk_p;
-   input wire	adcframe_p;
-	input wire	SPI_MOSI;
-	input wire	SPI_SCLK;
-	input wire	SPI_SS;
-   input wire	PMT_trigger;
+   input wire   CK50;
+   input wire   adcfastclk_p;
+   input wire   adcframe_p;
+        input wire      SPI_MOSI;
+        input wire      SPI_SCLK;
+        input wire      SPI_SS;
+   input wire   PMT_trigger;
    input wire [15:0] adcdata_p; // 16 serial lines
    input wire [31:0] TDC; // unused
-   output wire 	     ADC_SCLK1;
-   output wire 	     ADC_nCS1;
-   output wire 	     ADCCLK1_p;
-   output wire 	     ADCCLK2_p;
-   output wire 	     L1;
-   output wire 	     L0;
-	output wire			  SPI_MISO;
-   inout wire 	     ADC_SDIO1;
+   output wire       ADC_SCLK1;
+   output wire       ADC_nCS1;
+   output wire       ADCCLK1_p;
+   output wire       ADCCLK2_p;
+   output wire       L1;
+   output wire       L0;
+        output wire                       SPI_MISO;
+   inout wire        ADC_SDIO1;
 
    
-   reg 		     rst;
+   reg               rst;
 
    // input clock - 50 MHz 
-   wire 	     sysclk;
-   assign	sysclk = CK50;
+   wire              sysclk;
+   assign       sysclk = CK50;
 
    // output clocks to the two octal ADCs
    assign ADCCLK1_p = sysclk;
    assign ADCCLK2_p = sysclk;
 
-   wire [15:0] 	     dout;
-   wire 	     adc_ready; // output
+   wire [15:0]       dout;
+   wire              adc_ready; // output
 
-   wire 	     adc_flag;
-   reg [7:0] 	     adc_mode;
+   wire              adc_flag;
+   reg [7:0]         adc_mode;
 
    // ugh
 
@@ -81,146 +81,144 @@
    // synthesis read_comments_as_HDL on
    //   initial adc_mode = 8'h09;
    //   adc_spimaster adc_spimaster_inst(
-   //				    .sys_clk(sysclk),
-   //				    .reset_n(~rst),
+   //                               .sys_clk(sysclk),
+   //                               .reset_n(~rst),
    //      
-   //				    .adc_sclk(ADC_SCLK1),
-   //				    .adc_sdio(ADC_SDIO1),
-   //				    .adc_cs(ADC_nCS1),
+   //                               .adc_sclk(ADC_SCLK1),
+   //                               .adc_sdio(ADC_SDIO1),
+   //                               .adc_cs(ADC_nCS1),
    //      
-   //				    .adc_flag(adc_flag),
-   //				    .adc_mode(adc_mode),
-   //				    .adc_ready(adc_ready)
-   //				    );
+   //                               .adc_flag(adc_flag),
+   //                               .adc_mode(adc_mode),
+   //                               .adc_ready(adc_ready)
+   //                               );
    //
    //   
    // synthesis read_comments_as_HDL off
 
-   wire 	     fifo_empty;
-   wire [7:0]	     offset;
-   wire [7:0] 	     howmany;
+   wire              fifo_empty;
+   wire [7:0]        offset;
+   wire [7:0]        howmany;
    // module to contain the input from the digitizer channels.
    // configurable how many it controls by the CHAN variable.
    // howmany, offset should be made configurable - hardwired for now
    // DAVAIL and TRIGGER should also not be hardwired to their current widths.
    digi_many #(.CHAN(8)) digi_many_inst(
-					.RST(rst&adc_ready), 
-					.CK50(sysclk), 
-					.adc_clk(adcfastclk_p), 
-					.adc_frame(adcframe_p),
-					.adcdata_p(adcdata_p), 
-					.DOUT(dout), // output to remote
-					.ZYNQ_RD_REQUEST(1),
-					.GLBL_EMPTY(fifo_empty),
-					.howmany(howmany), // configuration
-					.offset(offset), // configuration
-					.DAVAIL(TDC[7:0]), // FIXME
-					.TRIGGER(TDC[16:8]) // FIXME -- replace with PMT_trigger?
-					);
+                                        .RST(rst&adc_ready), 
+                                        .CK50(sysclk), 
+                                        .adc_clk(adcfastclk_p), 
+                                        .adc_frame(adcframe_p),
+                                        .adcdata_p(adcdata_p), 
+                                        .DOUT(dout), // output to remote
+                                        .ZYNQ_RD_REQUEST(1),
+                                        .GLBL_EMPTY(fifo_empty),
+                                        .howmany(howmany), // configuration
+                                        .offset(offset), // configuration
+                                        .DAVAIL(TDC[7:0]), // FIXME
+                                        .TRIGGER(TDC[16:8]) // FIXME -- replace with PMT_trigger?
+                                        );
 
    // counter just to twiddle the LED on the digitzer
-   wire [31:0] 	     dcount;
+   wire [31:0]       dcount;
    bc_counter #(.BITS(32)) counter_inst0(
-					 .CLK(sysclk),
-					 .RST(rst), 
-					 .BC(dcount)
-					 );
+                                         .CLK(sysclk),
+                                         .RST(rst), 
+                                         .BC(dcount)
+                                         );
+/* -----\/----- EXCLUDED -----\/-----
    assign L0 = dcount[26]; // should be about 1 Hz
    assign L1 = dcount[25]; // should be 2x as fast as L1
+ -----/\----- EXCLUDED -----/\----- */
    
 
-//   //
-//   //   // LVDS output to the ZYNQ
-//   //   // the Z0 clock is the slow clock; its leading edge indicates the
-//   //   // MSB of the serial output stream.
-//   // synthesis read_comments_as_HDL on
-//   //   lvds_transmitter lvds_tx_inst(
-//   //				 .DIN(dout), 
-//   //				 .CLK(sysclk),
-//   //				 .O_D(Z0),
-//   //				 .O_CLK(Z0_CLK)
-//   //				 );
-//   // synthesis read_comments_as_HDL off
 
 
 
    // spi slave for ZYNQ communications
    localparam ZSPI_WORDSIZE = 8;
-   wire 	     SPI_done;
-	reg [ZSPI_WORDSIZE-1:0] SPI_datatomaster;
-	wire [ZSPI_WORDSIZE-1:0] SPI_datafrommaster;
-//   wire 		    done;
+   wire              SPI_done;
 
-	// TEST: initialize data to send to master
-	initial begin
-		SPI_datatomaster <= 8'b01010101;
-	end
+   // TEST: initialize data to send to master
 
    // RX: most recent received message
    // TX: next word to be sent
-   reg [ZSPI_WORDSIZE-1:0]  spi_rx_q, spi_tx_q;
-	
-	// TEST: just output to master whatever slave receives
-	always @ (SPI_done) begin
-		if (SPI_done) begin
-			SPI_datatomaster = SPI_datafrommaster;
-		end
-	end
+   reg [7:0] SPI_tx_reg; // to be transmitted
+   reg [7:0] SPI_rx_reg; // most recent received
+   reg [3:0] SPI_cmd; // SPI command
+   reg [3:0] SPI_addr; // address in SPI command
 
-   spi_slave #(.WORDSIZE(ZSPI_WORDSIZE)) spi_slave_inst(
-							.clk(sysclk),
-							.rst(rst),
-							.ss(SPI_SS), // ACTIVE LOW, input from master
-							.mosi(SPI_MOSI),
-							.miso(SPI_MISO),
-							.sck(SPI_SCLK),
-							.done(SPI_done),
-							.din(SPI_datatomaster),
-							.dout(SPI_datafrommaster)
-							);
-							
-   // store SPI output into spi_rx_q
+   wire [7:0] SPI_s; 
+
+   spi_slave 
+     #(.WORDSIZE(ZSPI_WORDSIZE)) spi_slave_inst(
+                                                .clk(sysclk),
+                                                .rst(rst),
+                                                .ss(SPI_SS), // ACTIVE LOW
+                                                .mosi(SPI_MOSI),
+                                                .miso(SPI_MISO),
+                                                .sck(SPI_SCLK),
+                                                .done(SPI_done),
+                                                .din(SPI_tx_reg),
+                                                .dout(SPI_s)
+                                                );
+
+   // store slave's data
    always @(posedge sysclk) begin
-      if ( rst == 1 ) begin
-			spi_rx_q <= {ZSPI_WORDSIZE{1'b0}};
-      end
-      else if (SPI_done == 1 ) begin
-			spi_rx_q <= SPI_datafrommaster;
+      if ( SPI_done ) begin
+         SPI_rx_reg <= SPI_s;
       end
    end
-	
-	// 16 control registers
+
+   // state machine outputs
+   wire       rd_select, wr_select, fifo_select, latch_cmd;
+
+   // handle the read and write 
+   always @(posedge sysclk ) begin
+      if ( rd_select) begin
+         SPI_tx_reg <= ctrl_regs[SPI_addr];
+      end 
+      else if ( SPI_done && wr_select ) begin
+         ctrl_regs[SPI_addr] <= SPI_rx_reg;
+      end
+      else if ( SPI_done && fifo_select ) begin
+	 // needs to set ZYNQ_RD_REQUEST for one clock cycle
+	 // BROKEN. Update SPI state machine to handle this.
+	 SPI_tx_reg <= dout[7:0];
+      end
+   end
+
+   // store the command and associated address data
+   always @(posedge sysclk ) begin
+      if ( latch_cmd ) begin
+         SPI_addr <= SPI_rx_reg[7:4]; 
+         SPI_cmd <= SPI_rx_reg[3:0];
+      end
+   end
+   assign L0 = rd_select;
+   assign L1 = wr_select;
+   
+   SPI_SM sm( // State machine for SPI slave on CycloneIII
+              .rd_select(rd_select),
+              .wr_select(wr_select),
+              .fifo_select(fifo_select),
+              .latch_cmd(latch_cmd),
+              .CMD(SPI_rx_reg[3:0]), // before they are latched
+              .DONE(SPI_done),
+              .FIFO_PK_SZ(FIFO_PK_SZ),   // number of words to send
+              .clk(sysclk),
+              .rst(rst) 
+              );
+   
+   
+   // 16 control registers
    reg [ZSPI_WORDSIZE-1:0]  ctrl_regs [15:0];
+   wire                     FIFO_PK_SZ;
 
    assign howmany = ctrl_regs[1] ;
    assign offset = ctrl_regs[2] ;
+   assign FIFO_PK_SZ = ctrl_regs[3];
 
-   // if write bit is not set, copy value of ctrl_regs to spi_tx_q
-   // should this also look at 'done'?
-   // hack: only look at bottom nibble
-   always @(posedge sysclk) begin
-      if ( spi_rx_q[ZSPI_WORDSIZE-1] == 0 ) begin // read enable
-			if ( spi_rx_q[4] == 0 ) begin // control registers
-				spi_tx_q <= ctrl_regs[spi_rx_q[3:0]];
-			end 
-			else begin // FIFO - just read it for now (should check data available)
-				if ( fifo_empty != 0 ) begin // fifo is not empty
-					spi_tx_q <= dout[ZSPI_WORDSIZE-1:0];
-				end else begin
-//					spi_tx_q <= 12'haaa; //default empty value
-					spi_tx_q <= {ZSPI_WORDSIZE{1'b1}}; // default empty value
-				end
-			end
-      end
-      else begin // write-enable
-			if ( spi_rx_q[4] == 0 ) begin
-				ctrl_regs[spi_rx_q[3:0]] <= SPI_datafrommaster;
-			end
-		end
-	end // always @ (posedge sysclk)
 
-   
-   //assign SPI_datatomaster = spi_tx_q;
    
 
    //--------------------------------------------------
@@ -229,11 +227,11 @@
    initial rst_cnt = 0;
    always @(posedge sysclk ) begin
       if ( rst_cnt < 5'd31 )
-			rst_cnt <= rst_cnt + 5'b1;
+                        rst_cnt <= rst_cnt + 5'b1;
       if ( rst_cnt < 5'd25) 
-			rst = 1;
+                        rst = 1;
       else
-			rst = 0;
+                        rst = 0;
    end
    
 endmodule
